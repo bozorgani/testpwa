35aeddcab48f04faba8bb04b88d23f9d2694f996
Mohammadamin Bozorgani <bozorgane.0312@gmail.com>
2025-02-09 02:43:50 +0330

    dsads3

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 
 const withPWA = require('next-pwa')({
-  //dest: 'public', // Output directory for service worker
-  register: true, // Automatically register service worker
-  skipWaiting: true, // Activate service worker immediately
-  scope: '/app',
+  dest: 'public', // Output directory for service worker
+  // register: true, // Automatically register service worker
+  // skipWaiting: true, // Activate service worker immediately
+  // scope: '/app',
   sw: 'service-worker.js',
-  disable: false,
+  disable: process.env.NODE_ENV === 'development',
   
 });
 
 module.exports = withPWA({
-  reactStrictMode: true,
+  reactStrictMode: false,
 });
 
