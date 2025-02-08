dc2a2c6642e9702ba3a0ff19b849af2a90cae840
Mohammadamin Bozorgani <bozorgane.0312@gmail.com>
2025-02-09 02:16:03 +0330

    123

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/192x192.png" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ next.config.js
@@ -0,0 +1,13 @@
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public', // Output directory for service worker
+  register: true, // Automatically register service worker
+  skipWaiting: true, // Activate service worker immediately
+  scope: '/app',
+  sw: 'service-worker.js',
+  disable: false
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
