175a2010d1ddc3176c7558bf7188154e6fa1aa1c
Mohammadamin Bozorgani <bozorgane.0312@gmail.com>
2025-02-09 22:49:27 +0330

    update 1403/11/21 22:49

--- next.config.js
+++ next.config.js
@@ -1,30 +1,28 @@
-
-const defaultRuntimeCaching = require("next-pwa/cache");
-const withPWA = require('next-pwa')({
-  dest: 'public', // Output directory for service worker
-   register: true, // Automatically register service worker
-   skipWaiting: true, // Activate service worker immediately
+// const defaultRuntimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public", // Output directory for service worker
+  register: true, // Automatically register service worker
+  skipWaiting: true, // Activate service worker immediately
   // scope: '/app',
-  sw: 'service-worker.js',
-  runtimeCaching: [
-    {
-      urlPattern: /\/api\/todoitem/,
-      method: "GET",
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "todoApp-api",
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 1 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    ...defaultRuntimeCaching,
-  ],
-  
+  // sw: 'service-worker.js',
+  // runtimeCaching: [
+  //   {
+  //     urlPattern: /\/api\/todoitem/,
+  //     method: "GET",
+  //     handler: "StaleWhileRevalidate",
+  //     options: {
+  //       cacheName: "todoApp-api",
+  //       expiration: {
+  //         maxEntries: 64,
+  //         maxAgeSeconds: 1 * 60 * 60, // 24 hours
+  //       },
+  //     },
+  //   },
+  //   ...defaultRuntimeCaching,
+  // ],
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
+  swcMinify: true,
 });
-
