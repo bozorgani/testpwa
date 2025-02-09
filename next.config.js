5d434198f10c381fca1d671b456526968eaa29f6
Mohammadamin Bozorgani <bozorgane.0312@gmail.com>
2025-02-09 15:40:49 +0330

    update service worker 1403/11/21 15:40

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
 
+const defaultRuntimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa')({
   dest: 'public', // Output directory for service worker
-  // register: true, // Automatically register service worker
-  // skipWaiting: true, // Activate service worker immediately
+   register: true, // Automatically register service worker
+   skipWaiting: true, // Activate service worker immediately
   // scope: '/app',
   sw: 'service-worker.js',
-  disable: process.env.NODE_ENV === 'development',
+  disable: false,
+  runtimeCaching: [
+    {
+      urlPattern: /\/api\/todoitem/,
+      method: "GET",
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "todoApp-api",
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 1 * 60 * 60, // 24 hours
+        },
+      },
+    },
+    ...defaultRuntimeCaching,
+  ],
   
 });
 
